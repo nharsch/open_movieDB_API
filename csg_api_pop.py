a0d6b3efd41ba158eb6562ded06eb25eea04f424
nharsch <nigelharsch@gmail.com>
2014-12-14 12:58:18 -0500

    updates to script

--- csg_api_pop.py
+++ csg_api_pop.py
@@ -1,10 +1,11 @@
 
 import requests
 import csv
+from sys import argv
 
 s = requests.session()
 
-csv_name = "test_sked.csv"
+script, csv_name = argv
 
 # params = {"t" : "The Dark Knight"}
 # r = s.get("http://www.omdbapi.com/", params=params)
@@ -25,14 +26,23 @@ with open(csv_name, 'rU') as csv_file:
     csv_reader = csv.reader(csv_file)
     movie_list = [row[0] for row in csv_reader] #make a list of the first elements of the CSV
 
+def dict_maker(movie_list):
+	dicter = []
+	for movie in movie_list:
+		metadata = title_exact_search(movie)
+		dicter.append({'Title' : movie, 
+						'Year' : metadata.get('Year', "not found"),
+						'Actors' : metadata.get('Actors', "not found"),
+						'Director' : metadata.get('Director', "not found"),
+						})
+	return dicter
 
+dicter = dict_maker(movie_list)
 
-dicter = [{'Title' : movie, 'Actors' : title_exact_search(movie).get('Actors', "not found")} for movie in movie_list]
-
-print dicter
+# print dicter
 
 with open(csv_name[:-4]+"_output.csv", 'w') as csvfile:
-	fieldnames = ['Title', 'Actors']
+	fieldnames = ['Title', 'Year', 'Actors', 'Director']
 	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 	writer.writeheader()
 	for row in dicter:
